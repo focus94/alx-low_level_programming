809d476fe92d9e0f4c3c77f15cbce1e5034bc5cc
Joseph Agbaibor <feeddfocus@gmail.com>
2023-06-18 13:57:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+char C;
+for (C = 'a'; C <= 'a'; c++)
+putchar(C);
+putchar('\n');
+return (0);
+}
