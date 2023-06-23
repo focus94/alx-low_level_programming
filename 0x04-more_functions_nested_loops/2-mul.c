8b0f2b6ad44d08e5cb07204a09250f88d1a5e3d5
Joseph Agbaibor <feeddfocus@gmail.com>
2023-06-22 19:39:47 -0700

    f

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check for checks for a didit (0 through 9).
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0.
+ */
+
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
