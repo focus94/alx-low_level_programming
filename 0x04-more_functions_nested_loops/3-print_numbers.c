a875f74e817f21771d645e9b4e302bc614a9e42b
Joseph Agbaibor <feeddfocus@gmail.com>
2023-06-23 03:44:02 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - check for check for a didit (0 through 0)
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+		int c:
+
+		for (c = 0; c < 10; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+}
